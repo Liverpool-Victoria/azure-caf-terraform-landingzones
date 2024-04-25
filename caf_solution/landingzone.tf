6d78e5fb1e313af7b81d31d6fd5e175df19f1366
Ross Clarke <ross.clarke@lv.com>
2024-04-25 14:26:42 +0100

    AB#5544 Upgrade CAF Terraform modules

--- caf_solution/landingzone.tf
+++ caf_solution/landingzone.tf
@@ -1,7 +1,8 @@
 module "solution" {
-  source  = "aztfmod/caf/azurerm"
-  version = "5.7.8"
+  #source  = "aztfmod/caf/azurerm"
+  #version = "5.7.8"
   # source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=main"
+  source  = "git::https://github.com/Liverpool-Victoria/terraform-azurerm-caf.git?ref=lvfs578"
 
   providers = {
     azurerm.vhub = azurerm.vhub
